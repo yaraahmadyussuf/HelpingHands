29d07c20a865b0017e6097eeafe3f3988d820960
nour <nourhisham464@gmail.com>
2026-08-13 01:26:16 +0300

    Second Part

--- HelpingHands/authentication.py
+++ HelpingHands/authentication.py
@@ -1,16 +1,22 @@
-from flask import Flask, request, render_template #takes me to the actual html page
-from werkzeug.security import generate_password_hash #turns text(pass) into a secure hash before we put it in the database
+#render_template: takes me to the actual html page
+#redirect: sends user to another page
+#url_for: find a flask route by its function name
+#session: remembers the user is logged in 3amel zy id card mo2aqat
+
+from flask import Flask, request, render_template, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash #turns text(pass) into a secure hash before we put it in the database
 from database import (email_exists, create_user, get_user_by_email, get_user_by_id)
 
 helping_hands = Flask(__name__)
+helping_hands.secret_key = "helping-hands-secret-key" #protects the session data
 
 
 @helping_hands.route("/") #going to the url to run the home function
 
 def home():
-    return render_template("homepage.html")
+    return render_template("homepage.html", user=session)
 
-
+# ===================== registration part =========================
 @helping_hands.route("/register", methods=["GET", "POST"]) #goes to the register page. get:shows the page. post:sends info to flask
 
 def register(): #request.method is how the brwoser contacts flask by either get or post
@@ -40,6 +46,37 @@ def register(): #request.method is how the brwoser contacts flask by either get 
         return "Registration Succesful!"
     
     return render_template("register.html")
+
+
+# ===================== login part =========================
+@helping_hands.route("/login", methods=["GET", "POST"])
+
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = get_user_by_email(email) #bagyb el user elly 3ndo this email
+
+        # mn 8eir el session the user will be fogotten b3d el request ma ykhls
+        if user and check_password_hash(user["password_hash"], password):
+            session["user_id"] = user["id"]
+            session["name"] = user["name"]
+            session["role"] = user["role"]
+
+            return redirect(url_for("home")) #sends user to home()
+
+        return "Invalid email or password."
+
+    return render_template("login.html")
+
+
+# ===================== logout part =========================
+@helping_hands.route("/logout")
+
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     helping_hands.run(debug=True)
