b7eebe71d4b428d74ef2557523b5b05368006359
gehadkhaled1107-hue <gehadkhaled1107@gmail.com>
2026-08-14 14:27:01 +0300

    app.py

--- HelpingHands/app.py
+++ HelpingHands/app.py
@@ -1,5 +1,4 @@
 import os   #==> -- to deal with file path (windows) -- >
-import sqlite3
 import time
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
